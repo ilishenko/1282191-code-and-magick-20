1402a116ef7825b51ae53a0014c4749fdfb0b31d
ilishenko <60015069+ilishenko@users.noreply.github.com>
2020-06-13 13:12:00 +0300

    Добавляет создание героя на основе рандомных данных

--- js/setup.js
+++ js/setup.js
@@ -3,8 +3,8 @@
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var QUANTITY_PLAYERS = '4';
-var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var userDialog = document.querySelector('.setup');
 var userDialogClose = userDialog.querySelector('.setup-close');
@@ -27,12 +27,22 @@ var randomElement = function (array) {
   return elementRandom;
 };
 
+var getWizad = function (name, surname, coat, eyes) {
+  var wizard = [];
+  wizard.name = randomElement(name) + ' ' + randomElement(surname);
+  wizard.coatColor = randomElement(coat);
+  wizard.eyesColor = randomElement(eyes);
+
+  return wizard;
+};
+
+
 var renderWizard = function () {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = randomElement(WIZARD_NAMES) + ' ' + randomElement(WIZARD_SURNAMES);
-  wizardElement.querySelector('.wizard-coat').style.fill = randomElement(coatColor);
-  wizardElement.querySelector('.wizard-eyes').style.fill = randomElement(eyesColor);
+  wizardElement.querySelector('.setup-similar-label').textContent = getWizad(WIZARD_NAMES, WIZARD_SURNAMES, COAT_COLOR, EYES_COLOR).name;
+  wizardElement.querySelector('.wizard-coat').style.fill = getWizad(WIZARD_NAMES, WIZARD_SURNAMES, COAT_COLOR, EYES_COLOR).coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = getWizad(WIZARD_NAMES, WIZARD_SURNAMES, COAT_COLOR, EYES_COLOR).eyesColor;
 
   return wizardElement;
 };
